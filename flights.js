c9206cb433f401cf60d0da1238ce26f0805a5dfa
zihchi <zihchi@icloud.com>
2025-07-25 19:03:10 +0800

    Add files via upload

---
+++ flights.js
@@ -0,0 +1,18 @@
+const flights = [
+  { flightNo: "205", route: "TPE/MFM" },
+  { flightNo: "206", route: "MFM/TPE" },
+  { flightNo: "846", route: "TPE/KMJ" },
+  { flightNo: "847", route: "KMJ/TPE" },
+  { flightNo: "771", route: "TPE/SIN" },
+  { flightNo: "772", route: "SIN/TPE" },
+  { flightNo: "840", route: "TPE/FUK" },
+  { flightNo: "841", route: "FUK/TPE" },
+  { flightNo: "850", route: "TPE/CTS" },
+  { flightNo: "851", route: "CTS/TPE" },
+  { flightNo: "862", route: "TPE/SDJ" },
+  { flightNo: "863", route: "SDJ/TPE" },
+  { flightNo: "713", route: "TPE/SGN" },
+  { flightNo: "714", route: "SGN/TPE" },
+  { flightNo: "804", route: "TPE/NRT" },
+  { flightNo: "805", route: "NRT/TPE" }
+];
